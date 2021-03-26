4d6ecd7b821ebf5d059dca9dc3e7a8852b58a3b8
Recep Zorlu <recepomerzorlu@gmail.com>
2021-03-27 00:48:38 +0100

    print added

--- main.py
+++ main.py
@@ -7,5 +7,15 @@ page_content = get_page_content("tt0068646")
 
 movie_keyword, keyword_movie = scrape_page_content("The Godfather", page_content, movie_keyword, keyword_movie)
 
+i = 0
+for keyword in movie_keyword:
+    print(keyword)
+    i += 1
+    if i > 50:
+        break
 
-print("hello")
+for movie in keyword_movie:
+    print(movie)
+    i += 1
+    if i > 100:
+        break
