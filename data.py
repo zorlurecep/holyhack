724e99c53fe4413e3f73dfa42a68f8cbf25c3f3c
muhammed-enes-koroglu <menes2000@hotmail.com>
2021-03-27 19:09:30 +0100

    can filter on year and content type, GUI side is to be yet implemented

--- data.py
+++ data.py
@@ -7,25 +7,31 @@ class Data:
     def __init__(self):
         print('Reading from the csv-file..')
         t1 = time.time()
-        # self.df = pd.read_csv(r'C:\Users\menes\Downloads\data.tsv', sep='\t',
-        #                       dtype={'tconst': 'string', 'titleType': 'string', 'primaryTitle': 'string',
-        #                              'originalTitle': 'string', 'startYear': 'string'},
-        #                       usecols=['tconst', 'titleType', 'primaryTitle', 'originalTitle', 'startYear',
-        #                                'genres'])
-        self.df = pd.read_csv(r'data.tsv', sep='\t',
-                              dtype={'tconst': 'string',
+        self.df = pd.read_csv(r'C:\Users\menes\Downloads\data.tsv', sep='\t',
+                              dtype={'tconst': 'string', 'titleType': 'string', 'primaryTitle': 'string',
                                      'originalTitle': 'string', 'startYear': 'string'},
-                              usecols=['tconst', 'originalTitle', 'startYear',
+                              usecols=['tconst', 'titleType', 'primaryTitle', 'originalTitle', 'startYear',
                                        'genres'])
+        # self.df = pd.read_csv(r'data.tsv', sep='\t',
+        #                       dtype={'tconst': 'string',
+        #                              'originalTitle': 'string', 'startYear': 'string'},
+        #                       usecols=['tconst', 'originalTitle', 'startYear',
+        #                                'genres'])
         t2 = time.time()
         print('reading time is: ', t2 - t1, '\n')
 
-    def title_to_id(self, title):
+    def title_to_id(self, title, content_type, year):
         """
+        :param content_type:
+        :param year:
         :param title: original title of a work (exact)
         :return: a set of imdb-id's of all works with exactly the same name
         """
         rows_with_title = self.df[self.df.originalTitle == title]
+        if content_type is not None:
+            rows_with_title = rows_with_title[rows_with_title.titleType == content_type]
+        if year is not None:
+            rows_with_title = rows_with_title[rows_with_title.startYear == year]
         return set(rows_with_title.tconst)
 
     def get_extended_names(self):
--- server.py
+++ server.py
@@ -6,14 +6,16 @@ from data import Data
 eel.init('web')
 
 
-def get_genres(data, movie_name):
-    genres = data.df[data.df.originalTitle == movie_name].genres
-    genres = set(genres)
+def get_genres(data, movie_ids):
+    all_genres = set()
+    for current_id in movie_ids:
+        genres = set(data.df[data.df.tconst == current_id].genres)
+        all_genres.update(genres)
     genres = { genre for string in genres for genre in string.split(',')}
 
     return list(genres)
 
-def search_by_name(data, movie_name):
+def search_by_name(data, movie_name, content_type, year):
     """
     :param data: panda dataframe
     :param movie_name: string
@@ -22,15 +24,14 @@ def search_by_name(data, movie_name):
     """
     if data.df[data.df.originalTitle == movie_name].empty:
         return None
-    get_genres(data, movie_name)
 
-    movie_ids = data.title_to_id(movie_name)
+    movie_ids = data.title_to_id(movie_name, content_type, year)
     tags = set()
     i = 1
     for movie_id in movie_ids:
         if i > 15:
             break
-        print(str(i) + 'th reference to', movie_name)
+        print(movie_id, str(i) + 'th reference to', movie_name)
         i += 1
         tags.update(imdb_scraper.get_keywords_from_id(movie_id))
 
@@ -43,12 +44,14 @@ def search_by_name(data, movie_name):
 
 
 @eel.expose
-def search(movieName, keywordName, contentType, year):
-    if keywordName is None:
-        print(movieName, keywordName, contentType, year)
-        tags = search_by_name(data, movieName)
+def search(movie_name, keyword, content_type, year):
+    year = None
+    content_type = 'movie'
+    if keyword is None:
+        print(movie_name, keyword, content_type, year)
+        tags = search_by_name(data, movie_name, content_type, year)
 
-        print("\nTags related to " + movieName + " are: \n", tags)
+        print("\nTags related to " + movie_name + " are: \n", tags)
         return tags
 
 
